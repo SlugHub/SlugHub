af7663cdd97f40fdccf7ac28d7c20a3f349cc0d6
Alex Brown <Alex.Brown@asos.com>
2018-07-23 15:08:31 +0100

    added GroupingKey capability Can differentiate across groupingKeys (useful for multi-tenant apps etc...)

--- src/SlugStore/InMemorySlugStore.cs
+++ src/SlugStore/InMemorySlugStore.cs
@@ -1,19 +1,56 @@
 ﻿using System.Collections.Concurrent;
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SlugHub.Tests")]
 
 namespace SlugHub.SlugStore
 {
     public class InMemorySlugStore : ISlugStore
     {
-        internal static ConcurrentDictionary<string, Slug> Cache = new ConcurrentDictionary<string, Slug>();
+        private const string DefaultCacheGroupingKey = "Default";
+
+        private static readonly ConcurrentDictionary<string, ConcurrentDictionary<string, Slug>> GroupedCache;
+
+        static InMemorySlugStore()
+        {
+            GroupedCache = new ConcurrentDictionary<string, ConcurrentDictionary<string, Slug>>();
+
+            if (!GroupedCache.ContainsKey(DefaultCacheGroupingKey))
+                GroupedCache.TryAdd(DefaultCacheGroupingKey, new ConcurrentDictionary<string, Slug>());
+        }
 
         public bool Exists(string slug)
         {
-            return Cache.ContainsKey(slug);
+            return Exists(slug, null);
+        }
+
+        public bool Exists(string slug, string groupingKey)
+        {
+            if (string.IsNullOrEmpty(groupingKey))
+                groupingKey = DefaultCacheGroupingKey;
+
+            if (!GroupedCache.ContainsKey(groupingKey))
+                return false;
+
+            var cache = GroupedCache[groupingKey];
+
+            return cache.ContainsKey(slug);
         }
 
         public void Store(Slug slug)
         {
-            Cache.TryAdd(slug.Value, slug);
+            var groupingKey = string.IsNullOrEmpty(slug.GroupingKey)
+                ? DefaultCacheGroupingKey
+                : slug.GroupingKey;
+
+            if (!GroupedCache.ContainsKey(groupingKey))
+                GroupedCache.TryAdd(groupingKey, new ConcurrentDictionary<string, Slug>());
+
+            var cache = GroupedCache[groupingKey];
+            cache.TryAdd(slug.Value, slug);
+        }
+
+        internal void Clear()
+        {
+            GroupedCache.Clear();
         }
     }
 }
